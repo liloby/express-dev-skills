ece90a31ef6c84de531d40c65b9ea666fed73540
Oby <obyli214@gmail.com>
2022-06-16 16:46:09 -0700

    done main objectives

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,7 +2,12 @@ const Skill = require('../models/skill')
 
 module.exports = {
     index,
-    show
+    show,
+    new: newSkill,
+    create,
+    delete: deleteSkill,
+    edit,
+    update
 }
 
 function index(req, res) {
@@ -15,4 +20,29 @@ function show(req, res) {
     res.render('skills/show', {
         skill: Skill.getOne(req.params.id)
     })
+}
+
+function newSkill(req, res) {
+    res.render('skills/new')
+}
+
+function create(req, res) {
+    Skill.create(req.body)
+    res.redirect('/skills')
+}
+
+function deleteSkill(req, res) {
+    Skill.deleteOne(req.params.id)
+    res.redirect('/skills')
+}
+
+function edit(req, res) {
+    res.render('skills/edit', {
+        skill: Skill.getOne(req.params.id)
+    })
+}
+
+function update(req, res) {
+    Skill.update(req.params.id, req.body)
+    res.redirect(`/skills/${req.params.id}`)
 }
--- models/skill.js
+++ models/skill.js
@@ -1,14 +1,17 @@
 const skills = [
     {id: 1, skill: 'HTML', complete: true},
     {id: 2, skill: 'CSS', complete: true},
-    {id: 3, skill: 'Javascript', complete: true},
-    {id: 4, skill: 'Express', complete: false},
-    {id: 5, skill: 'React', complete: false}
+    {id: 3, skill: 'JAVASCRIPT', complete: true},
+    {id: 4, skill: 'EXPRESS', complete: false},
+    {id: 5, skill: 'REACT', complete: false}
   ];
   
   module.exports = {
     getAll,
-    getOne
+    getOne,
+    create,
+    deleteOne,
+    update
   };
   
   function getAll() {
@@ -18,4 +21,22 @@ const skills = [
   function getOne(id) {
     id = Number(id)
     return skills.find(skill => skill.id === id)
+  }
+
+  function create(skill) {
+    skill.id = skills.length + 1
+    skill.complete = false
+    skills.push(skill)
+  }
+
+  function deleteOne(id) {
+    id = Number(id)
+    const idx = skills.findIndex(skill => skill.id === id)
+    skills.splice(idx, 1)
+  }
+
+  function update(id, skill) {
+    id = Number(id)
+    const skillUpdate = skills.find(skill => skill.id === id)
+    skillUpdate.skill = skill.skill
   }
